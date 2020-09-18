00dd9f49ce13c69281dce1910b864cd22c894616
João Dantas <joaotitordantas@gmail.com>
2020-09-18 15:23:24 -0300

    alteração em diversas páginas, criação de login mais uma vez

--- resources/views/Home/entrar.blade.php
+++ resources/views/Home/entrar.blade.php
@@ -13,18 +13,19 @@
 
 
 
-<
-<br>
+
 
 
 <div class="container ">
+   
+   <div class="container d-flex justify-content-center">
     <form method="POST" action="{{ route('entrar') }}">
         @csrf
 
         <div class="form-group row">
             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail ') }}</label>
 
-            <div class="col-md-3">
+            <div class="col-md-8">
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                 @error('email')
@@ -38,7 +39,7 @@
         <div class=" form-group row">
             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
-            <div class="col-md-3">
+            <div class="col-md-8">
                 <input id="password" type="password" class="form-control @error('Senha') is-invalid @enderror" name="Senha" required autocomplete="current-password">
 
                 @error('password')
@@ -63,7 +64,7 @@
 
         <div class=" container form-group row mb-0">
             <div class="col-md-3 offset-md-4">
-                <button type="submit" class="btn btn-success">
+                <button type="submit" class="btn btn-lg btn-success">
                     {{ __('Entrar') }}
                 </button>
 
@@ -75,7 +76,7 @@
             </div>
         </div>
     </form>
-
+</div>
     <div class="container d-flex justify-content-center" style="margin-bottom:10%; margin-top: 10%; ">
             
         <a class="btn btn-success" href="cadastro-pessoa-1" style="width: 50%; font-size: 25px; padding: 2%;" id="entrar">Não tem conta? Cadastre-se aqui </a>
